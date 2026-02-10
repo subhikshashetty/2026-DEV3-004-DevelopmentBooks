fe5dc39536633c1b6805ca14218934232f503edd
Subhiksha Shetty <subhiksha.shetty@gmail.com>
2026-02-10 20:21:07 +0100

    Initial Project Setup

---
+++ bookShoppingCart/src/main/java/com/bnppf/bookShoppingCart/BookShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package com.bnppf.bookShoppingCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShoppingCartApplication.class, args);
+	}
+
+}
---
+++ bookShoppingCart/src/test/java/com/bnppf/bookShoppingCart/BookShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnppf.bookShoppingCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
