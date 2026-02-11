801a5f7267d28c8a4a65666b776ece4a1502c659
Subhiksha Shetty <subhiksha.shetty@gmail.com>
2026-02-12 00:43:13 +0100

    RED: Added test to handle 400 bad request with error response

--- bookShoppingCart/src/test/java/com/bnppf/bookShoppingCart/controller/ShoppingCartControllerTest.java
+++ bookShoppingCart/src/test/java/com/bnppf/bookShoppingCart/controller/ShoppingCartControllerTest.java
@@ -10,10 +10,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.core.StringContains.containsString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ShoppingCartController.class)
 @AutoConfigureMockMvc
@@ -43,4 +43,24 @@ public class ShoppingCartControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("95.0"));
     }
+
+    @Test
+    void shouldReturn400WhenInvalidEnumProvided() throws Exception {
+
+        String invalidJson = """
+                {
+                  "books": ["CLEA1_CODE"]
+                }
+                """;
+
+        mockMvc.perform(post("/api/bookprice/total")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.error").value("Invalid Enum Value"))
+                .andExpect(jsonPath("$.message")
+                        .value(containsString("Invalid book name")));
+    }
 }
+
