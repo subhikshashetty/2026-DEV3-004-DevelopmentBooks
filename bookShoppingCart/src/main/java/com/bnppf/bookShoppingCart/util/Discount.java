2c64145f09ee465376b40f44510188cb79a36aa2
Subhiksha Shetty <subhiksha.shetty@gmail.com>
2026-02-11 15:48:09 +0100

    REFACTOR: Removed unused import statements

--- bookShoppingCart/src/main/java/com/bnppf/bookShoppingCart/util/Discount.java
+++ bookShoppingCart/src/main/java/com/bnppf/bookShoppingCart/util/Discount.java
@@ -1,9 +1,5 @@
 package com.bnppf.bookShoppingCart.util;
 
-import com.bnppf.bookShoppingCart.enums.Book;
-
-import java.util.List;
-
 public interface Discount {
     double applyDiscount(double actualPrice);
     boolean isApplicable(int groupSize);
