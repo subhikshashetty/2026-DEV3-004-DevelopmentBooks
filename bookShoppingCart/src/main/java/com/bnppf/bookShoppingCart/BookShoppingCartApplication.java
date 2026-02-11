71ff1d3c157bc74cf7c190e22c295e7bb2103701
Subhiksha Shetty <subhiksha.shetty@gmail.com>
2026-02-12 00:35:31 +0100

    GREEN: Added rest endpoint to calculate the total cost with discount

--- bookShoppingCart/src/main/java/com/bnppf/bookShoppingCart/BookShoppingCartApplication.java
+++ bookShoppingCart/src/main/java/com/bnppf/bookShoppingCart/BookShoppingCartApplication.java
@@ -1,8 +1,17 @@
 package com.bnppf.bookShoppingCart;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Book Shopping Cart API",
+				version = "1.0",
+				description = "API documentation for calculating the total discounts for the book"
+		)
+)
 @SpringBootApplication
 public class BookShoppingCartApplication {
 
--- bookShoppingCart/src/main/java/com/bnppf/bookShoppingCart/controller/ShoppingCartController.java
+++ bookShoppingCart/src/main/java/com/bnppf/bookShoppingCart/controller/ShoppingCartController.java
@@ -1,7 +1,33 @@
 package com.bnppf.bookShoppingCart.controller;
 
+import com.bnppf.bookShoppingCart.model.ShoppingCartRequest;
+import com.bnppf.bookShoppingCart.service.BookShoppingCartPriceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/bookprice")
+@Tag(name = "Book Price Calculator API", description = "Calculate total price with discounts for selected books")
 public class ShoppingCartController {
+
+    private final BookShoppingCartPriceService bookShoppingCartPriceService;
+
+
+    public ShoppingCartController(BookShoppingCartPriceService bookShoppingCartPriceService) {
+        this.bookShoppingCartPriceService = bookShoppingCartPriceService;
+    }
+
+    @PostMapping("total")
+    @Operation(
+            summary = "Calculate total price",
+            description = "Calculates total price of books considering discounts for combinations")
+    public ResponseEntity<Double> getTotalPrice(@RequestBody ShoppingCartRequest books) {
+        Double totalPrice = this.bookShoppingCartPriceService.calculateTotalPrice(books.getBooks());
+        return ResponseEntity.ok().body(totalPrice);
+    }
 }
