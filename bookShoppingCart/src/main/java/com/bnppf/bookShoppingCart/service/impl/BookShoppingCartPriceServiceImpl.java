e1a41c1355cf94db918a5dc1dd7b5311ed2f3d6e
Subhiksha Shetty <subhiksha.shetty@gmail.com>
2026-02-11 15:47:30 +0100

    REFACTOR:Moved the Book Grouping logic into separate class BookCollectionAggregator

--- bookShoppingCart/src/main/java/com/bnppf/bookShoppingCart/service/impl/BookShoppingCartPriceServiceImpl.java
+++ bookShoppingCart/src/main/java/com/bnppf/bookShoppingCart/service/impl/BookShoppingCartPriceServiceImpl.java
@@ -2,14 +2,12 @@ package com.bnppf.bookShoppingCart.service.impl;
 
 import com.bnppf.bookShoppingCart.enums.Book;
 import com.bnppf.bookShoppingCart.service.BookShoppingCartPriceService;
+import com.bnppf.bookShoppingCart.util.BookCollectionAggregator;
 import com.bnppf.bookShoppingCart.util.Discount;
 import com.bnppf.bookShoppingCart.util.DiscountFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class BookShoppingCartPriceServiceImpl implements BookShoppingCartPriceService {
@@ -21,41 +19,6 @@ public class BookShoppingCartPriceServiceImpl implements BookShoppingCartPriceSe
         this.discounts = DiscountFactory.getDiscounts();
     }
 
-    private static void decrementOrRemove(Book title, Map<Book, Integer> map) {
-        map.computeIfPresent(title, (k, v) -> v - 1);
-        if (map.get(title) != null && map.get(title) <= 0) {
-            map.remove(title);
-        }
-    }
-
-    //This method will check the group size, If the group size is 5 or 3 change it to 4 and 4 which is optimal
-    private static void getOptimalGroups(List<List<Book>> groupedBooks) {
-        int indexOfGroupSizeFive = -1;
-        int indexOfGroupSizeThree = -1;
-        for (int i = 0; i < groupedBooks.size(); i++) {
-            int size = groupedBooks.get(i).size();
-            if (size == 3 && indexOfGroupSizeThree == -1) {
-                indexOfGroupSizeThree = i;
-            } else if (size == 5 && indexOfGroupSizeFive == -1) {
-                indexOfGroupSizeFive = i;
-            }
-        }
-
-        if (indexOfGroupSizeFive != -1 && indexOfGroupSizeThree != -1) {
-            List<Book> groupSixeFiveList = groupedBooks.get(indexOfGroupSizeFive);
-            List<Book> groupSixeThisList = groupedBooks.get(indexOfGroupSizeThree);
-
-            for (Book book : new ArrayList<>(groupSixeFiveList)) {
-                boolean alreadyExists = groupSixeThisList.stream()
-                        .anyMatch(b -> b.getTitle().equals(book.getTitle()));
-                if (!alreadyExists) {
-                    groupSixeFiveList.remove(book);
-                    groupSixeThisList.add(book);
-                    break;
-                }
-            }
-        }
-    }
 
     @Override
     public double calculateTotalPrice(List<Book> books) {
@@ -66,20 +29,7 @@ public class BookShoppingCartPriceServiceImpl implements BookShoppingCartPriceSe
         if (books.size() == 1) {
             return BASE_BOOK_PRICE;
         } else {
-            Map<Book, Integer> bookCountsByTitle = new HashMap<>();
-            List<List<Book>> groupedBooks = new ArrayList<>();
-            for (Book book : books) {
-                bookCountsByTitle.put(book, bookCountsByTitle.getOrDefault(book, 0) + 1);
-            }
-            while (!bookCountsByTitle.isEmpty()) {
-                List<Book> group = new ArrayList<>();
-                for (Book books1 : new ArrayList<>(bookCountsByTitle.keySet())) {
-                    group.add(books1);
-                    decrementOrRemove(books1, bookCountsByTitle);
-                }
-                groupedBooks.add(group);
-            }
-            getOptimalGroups(groupedBooks);
+            List<List<Book>> groupedBooks = BookCollectionAggregator.getBookCollection(books);
             totalPrice = groupedBooks.stream()
                     .mapToDouble(this::calculateDiscountPrice)
                     .sum();
