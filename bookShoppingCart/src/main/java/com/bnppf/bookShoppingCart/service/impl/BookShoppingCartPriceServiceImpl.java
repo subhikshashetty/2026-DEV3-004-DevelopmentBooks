7c5431bdbe0a95c4819de5be06c86da78eb0a24b
Subhiksha Shetty <subhiksha.shetty@gmail.com>
2026-02-11 10:56:31 +0100

    GREEN: Added logic in BookShoppingCartPriceServiceImpl calculateTotalPrice method for empty basket

--- bookShoppingCart/src/main/java/com/bnppf/bookShoppingCart/service/impl/BookShoppingCartPriceServiceImpl.java
+++ bookShoppingCart/src/main/java/com/bnppf/bookShoppingCart/service/impl/BookShoppingCartPriceServiceImpl.java
@@ -1,8 +1,18 @@
 package com.bnppf.bookShoppingCart.service.impl;
 
+import com.bnppf.bookShoppingCart.enums.Book;
 import com.bnppf.bookShoppingCart.service.BookShoppingCartPriceService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookShoppingCartPriceServiceImpl implements BookShoppingCartPriceService {
+    @Override
+    public double calculateTotalPrice(List<Book> books) {
+        if (books == null || books.isEmpty()) {
+            return 0.0;
+        }
+        return 50.0;
+    }
 }
